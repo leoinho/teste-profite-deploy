c7539b5737389fc31b48364421267d1e72901256
unknown <leoinhosagaz@hotmail.com>
2019-03-22 15:24:03 -0300

    Updates

---
+++ precache-manifest.87fbd06fd70fe19ca66376ecf36d4897.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e54d1744b7b9b2ae1633e81e59112fb",
+    "url": "/teste-profite-deploy/static/media/LupaHeader.4e54d174.svg"
+  },
+  {
+    "revision": "e03b242ce34fd07d5792",
+    "url": "/teste-profite-deploy/static/css/main.b98c2d9d.chunk.css"
+  },
+  {
+    "revision": "c19af832477f70c23194",
+    "url": "/teste-profite-deploy/static/js/runtime~main.db4a0fcc.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/teste-profite-deploy/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b13113f5322c6dc55b41",
+    "url": "/teste-profite-deploy/static/js/2.def8d527.chunk.js"
+  },
+  {
+    "revision": "efcf99566895f216f197f04d90fa6ec8",
+    "url": "/teste-profite-deploy/static/media/Banner.efcf9956.png"
+  },
+  {
+    "revision": "da0fcba2cefd9c1f1b926775daee5e38",
+    "url": "/teste-profite-deploy/static/media/BannerMobi.da0fcba2.png"
+  },
+  {
+    "revision": "6c97e3380ef2343e1034d6e1b96eb295",
+    "url": "/teste-profite-deploy/static/media/Carrinho.6c97e338.svg"
+  },
+  {
+    "revision": "ebc5e10b54f7b90f42049fba0a40c861",
+    "url": "/teste-profite-deploy/static/media/Star.ebc5e10b.svg"
+  },
+  {
+    "revision": "7fb4f35dfc4f68a239072feae4daa77f",
+    "url": "/teste-profite-deploy/static/media/TenisPequeno.7fb4f35d.png"
+  },
+  {
+    "revision": "a019970708b74c6493fd72f14d115eee",
+    "url": "/teste-profite-deploy/static/media/Email.a0199707.svg"
+  },
+  {
+    "revision": "911c084fa326c143aa2cc95ded4ac7d4",
+    "url": "/teste-profite-deploy/static/media/Fone.911c084f.svg"
+  },
+  {
+    "revision": "ed28d9b242fc6302a050cf1c31b16347",
+    "url": "/teste-profite-deploy/static/media/LogoHeader.ed28d9b2.svg"
+  },
+  {
+    "revision": "e03b242ce34fd07d5792",
+    "url": "/teste-profite-deploy/static/js/main.ea6d376f.chunk.js"
+  },
+  {
+    "revision": "98a8b01b169f919d34347e6558e9dc01",
+    "url": "/teste-profite-deploy/static/media/AvataHeader.98a8b01b.svg"
+  },
+  {
+    "revision": "3731037c87ed2c051c934d3f84a40d6a",
+    "url": "/teste-profite-deploy/static/media/MinicartHeader.3731037c.svg"
+  },
+  {
+    "revision": "d2b9b589aefbddcf8978fc4fb509e4b1",
+    "url": "/teste-profite-deploy/static/media/IdiomasHeader.d2b9b589.svg"
+  },
+  {
+    "revision": "41e5a434125787bdd9157ded4b5b18e5",
+    "url": "/teste-profite-deploy/static/media/MenuMobiHeader.41e5a434.svg"
+  },
+  {
+    "revision": "4a84785f4bf835ddeed6051924d50493",
+    "url": "/teste-profite-deploy/static/media/Tenis.4a84785f.png"
+  },
+  {
+    "revision": "4b8d60b75c649989c28a313829c4ee9a",
+    "url": "/teste-profite-deploy/static/media/DescontoMobi.4b8d60b7.svg"
+  },
+  {
+    "revision": "ec7bf2a5fba418a066657da6f0e99de3",
+    "url": "/teste-profite-deploy/static/media/LogosFooter.ec7bf2a5.svg"
+  },
+  {
+    "revision": "8004e36d1d5b8d6ac9787cbd738dfa74",
+    "url": "/teste-profite-deploy/static/media/LogosFooterMobi.8004e36d.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/teste-profite-deploy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/teste-profite-deploy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/teste-profite-deploy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b13113f5322c6dc55b41",
+    "url": "/teste-profite-deploy/static/css/2.eae399aa.chunk.css"
+  },
+  {
+    "revision": "687f8d61fc4c4ffe84b29fbe2af89efc",
+    "url": "/teste-profite-deploy/index.html"
+  }
+];
